d7bcc65c8fb4418610792b1d03af8b54bfee7128
Duartemario501 <52871614+Duartemario501@users.noreply.github.com>
2024-06-07 14:17:51 -0600

    insert

--- db.sql
+++ db.sql
@@ -80,3 +80,28 @@ CREATE TABLE IF NOT EXISTS materia_prima_analisis(
 );
 
 ALTER TABLE materia_prima_analisis ADD CONSTRAINT `materia_prima_analisis_materia_prima_fk` FOREIGN KEY(id_materia_prima) REFERENCES materia_prima(id);
+
+INSERT INTO vehiculo (placa,tipo,activo, capacidad)VALUES('C-98456','Plataforma','1','280'),
+INSERT INTO vehiculo (placa,tipo,activo, capacidad)VALUES('C-98326','Plataforma','1','280'),
+INSERT INTO vehiculo (placa,tipo,activo, capacidad)VALUES('C-92346','pickat','1','20'),
+INSERT INTO vehiculo (placa,tipo,activo, capacidad)VALUES('C-91236','Plataforma','1','150'),
+INSERT INTO vehiculo (placa,tipo,activo, capacidad)VALUES('C-91234','Plataforma','1','100'),
+INSERT INTO vehiculo (placa,tipo,activo, capacidad)VALUES('C-92346','Sedan','1','5'),
+INSERT INTO MOTORISTA(dui, nombres, apellidoos)VALUES('04789878-0','Juan Carlos','Perez Galan'),
+INSERT INTO MOTORISTA(dui, nombres, apellidoos)VALUES('04523278-3','Humberto Alejandro','Rodriguez'),
+INSERT INTO MOTORISTA(dui, nombres, apellidoos)VALUES('05235278-2','Eduardo Carlos','Cardona, Pacheco'),
+INSERT INTO MOTORISTA(dui, nombres, apellidoos)VALUES('05653878-3','Efrain Ernesto','Mendoza Avila'),
+INSERT INTO MOTORISTA(dui, nombres, apellidoos)VALUES('04782378-1','Antonio Eduardo','Payes Mendoza'),
+INSERT INTO materia_prima(tipo,proveedor,descripcion)VALUES('Maiz Amarrillo perla de oro','Impor S.A de C.V',"Grano de maiz amarillo"),
+INSERT INTO materia_prima(tipo,proveedor,descripcion)VALUES('Maiz Amarrillo americano','texas S.A de C.V',"Grano de maiz americano grande"),
+INSERT INTO materia_prima(tipo,proveedor,descripcion)VALUES('Soja Amarrilla','jgs S.A de C.V',"Grano de soja amarillo"),
+INSERT INTO materia_prima(tipo,proveedor,descripcion)VALUES('Soja verde','indesa S.A de C.V',"Grano de soja verde"),
+INSERT INTO materia_prima(tipo,proveedor,descripcion)VALUES('Maiz Amarrillo nacional','travelmac S.A de C.V',"Grano de maiz nacional"),
+INSERT INTO materia_prima_analisis(lote, id_materia_prima,cantida_muestra,humedad,impureza,numero_pesa,tamiz_1,tamiz_2,tamiz_3,tamiz_4, estado)VALUES('150','1','1','0.10','o,03','65435','0.94','0.03','0.02','0,01','Aprovado'),
+INSERT INTO materia_prima_analisis(lote, id_materia_prima,cantida_muestra,humedad,impureza,numero_pesa,tamiz_1,tamiz_2,tamiz_3,tamiz_4, estado)VALUES('152','2','1','0.10','o,03','65436','0.97','0.03','0.00','0,00','Aprovado'),
+INSERT INTO materia_prima_analisis(lote, id_materia_prima,cantida_muestra,humedad,impureza,numero_pesa,tamiz_1,tamiz_2,tamiz_3,tamiz_4, estado)VALUES('153','2','1','0.10','o,02','65437','0.94','0.03','0.03','0,00','Aprovado'),
+INSERT INTO materia_prima_analisis(lote, id_materia_prima,cantida_muestra,humedad,impureza,numero_pesa,tamiz_1,tamiz_2,tamiz_3,tamiz_4, estado)VALUES('154','3','1','0.10','o,02','65438','0.93','0.03','0.03','0,01','Aprovado'),
+INSERT INTO materia_prima_analisis(lote, id_materia_prima,cantida_muestra,humedad,impureza,numero_pesa,tamiz_1,tamiz_2,tamiz_3,tamiz_4, estado)VALUES('155','3','1','0.10','o,05','65439','0.99','0.01','0.00','0,00','Aprovado'),
+INSERT INTO materia_prima_analisis(lote, id_materia_prima,cantida_muestra,humedad,impureza,numero_pesa,tamiz_1,tamiz_2,tamiz_3,tamiz_4, estado)VALUES('156','5','1','0.10','o,06','65440','0.90','0.05','0.04','0,01','Aprovado')
+
+
